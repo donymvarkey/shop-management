8cf0426501fcac7edf1b21f0b4bab897c771abed
Dony M Varkey <31372381+donymvarkey@users.noreply.github.com>
2020-10-09 13:00:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const options ={
     secret: process.env.secret,
     port : process.env.PORT,
     mongodb: {
-        uri : process.env.MONGO_URL 
+        uri : process.env.MONGO_URL || "mongodb+srv://admin:9rM8vZCxFDoyCX3n@cluster0.3to02.mongodb.net/shop-mgmt?retryWrites=true&w=majority"
     },
 }
 
